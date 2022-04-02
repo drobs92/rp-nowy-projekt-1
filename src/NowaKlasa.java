abf6a9317b9868786d16f4f5b4e15b37f138d42b
Robert Piechowski <robert.t.piechowski@gmail.com>
2022-04-02 12:24:09 +0200

    Drugi wpis w historii plików

---
+++ src/NowaKlasa.java
@@ -0,0 +1,10 @@
+/* komentarz wieloliniowy -można pisać w kilku linijkach*/
+
+/**
+ * Ten komentarz jest częścią dokumentacji
+ */
+public class NowaKlasa {//do zrobienia
+    //TODO: kupić mleko
+//    komentarz
+
+}
